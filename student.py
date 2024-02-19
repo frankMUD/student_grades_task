44cb2ed334eae6a1cd8f046d0a1cd45b6517971a
frankMUD <160316621+frankMUD@users.noreply.github.com>
2024-02-19 17:31:27 +0200

    Update student.py

--- student.py
+++ student.py
@@ -5,18 +5,21 @@ class Student():
         self.age = age
         self.name = name
         self.gender = gender
-        self.grades  = grades
-   
-    #  Method to calculate average grade
+        self.grades = grades
+
+    # Method to calculate average grade
     def compute_average(self):
-        average = sum(self.grades)/len(self.grades)
+        average = sum(self.grades) / len(self.grades)
         print("The average for student " + self.name + " is " + str(average))
 
+    # Function to find highest grade for the student
+    def find_highest_grade(self):
+        highest_grade = max(self.grades)
+        print("The highest grade for student " + self.name + " is " + str(highest_grade))
+
 # Create Student objects
-philani  = Student(20, "Philani Sithole", "Male", [64,65])
-sarah = Student(19, "Sarah Jones", "Female", [82,58])
+philani = Student(20, "Philani Sithole", "Male", [64, 65])
+sarah = Student(19, "Sarah Jones", "Female", [82, 58])
 
-# Method call
+# Method calls
 sarah.compute_average()
-
-# Add in new objects and logic here, and above to test your understanding.
